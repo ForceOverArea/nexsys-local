5678a3bb06e3a7ffe057831890ff4a1cd5416766
Grant C <70045551+ForceOverArea@users.noreply.github.com>
2022-12-30 14:56:43 -0600

    added `pkg` contents

---
+++ pkg/nexsys_js.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} system
+* @param {number} tolerance
+* @param {number} max_iterations
+* @param {boolean} allow_nonconvergence
+* @returns {string}
+*/
+export function solve_js(system: string, tolerance: number, max_iterations: number, allow_nonconvergence: boolean): string;
---
+++ pkg/nexsys_js.js
@@ -0,0 +1,2 @@
+import * as wasm from "./nexsys_js_bg.wasm";
+export * from "./nexsys_js_bg.js";
